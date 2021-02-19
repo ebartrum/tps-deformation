c5e19a4638922000fb3892fd77e3f695ad1859dc
Ed Bartrum <edward.bartrum@gmail.com>
2021-02-19 23:20:32 +0000

    Switch source and deformation grid

--- example.py
+++ example.py
@@ -1,6 +1,9 @@
 import tps
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
+
+#TODO: this needs to work on a batch
 
 samp = np.linspace(-2, 2, 4)
 xx, yy = np.meshgrid(samp, samp)
@@ -13,7 +16,7 @@ yy[:, [0, 3]] *=2
 deform_xy = np.stack([xx, yy], axis=2).reshape(-1, 2)
 
 # get coefficient, use class
-trans = tps.TPS(source_xy, deform_xy)
+trans = tps.TPS(deform_xy, source_xy)
 
 # make other points a left-bottom to upper-right line on source surface
 samp2 = np.linspace(-1.8, 1.8, 10)
@@ -22,10 +25,10 @@ test_xy = np.tile(samp2, [2, 1]).T
 # get transformed points
 transformed_xy = trans(test_xy)
 
-plt.scatter(source_xy[:,0], source_xy[:,1])
+plt.scatter(deform_xy[:,0], deform_xy[:,1])
 plt.scatter(test_xy[:,0], test_xy[:,1])
 plt.show()
 
-plt.scatter(deform_xy[:,0], deform_xy[:,1])
+plt.scatter(source_xy[:,0], source_xy[:,1])
 plt.scatter(transformed_xy[:,0], transformed_xy[:,1])
 plt.show()
